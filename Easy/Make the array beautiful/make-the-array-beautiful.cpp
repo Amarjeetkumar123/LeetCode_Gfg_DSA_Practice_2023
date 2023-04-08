bbf4e650173c9c886cead73921b748787723b0fc
Amarjeet Kumar <76771020+Amarjeetkumar123@users.noreply.github.com>
2023-04-08 12:11:23 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Make the array beautiful/make-the-array-beautiful.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    vector<int> makeBeautiful(vector<int> arr) {
+        
+        vector<int>s;
+        for(auto num:arr){
+            if(s.size()>0 && ((num>=0 && s.back()<0) || (num<0 && s.back()>=0)))
+                s.pop_back();
+            else
+                s.push_back(num);
+        }
+        return s;
+
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> arr(n);
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+
+        Solution obj;
+        vector<int> res = obj.makeBeautiful(arr);
+        for (int i = 0; i < res.size(); i++) {
+            cout << res[i] << " ";
+        }
+
+        cout << "\n";
+    }
+}
+// } Driver Code Ends
